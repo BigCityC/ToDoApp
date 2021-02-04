0e102b8d114d7a8baa99b1506608cf903ee68d1c
BigCityC <courtneywalsh05@gmail.com>
2021-02-04 16:57:51 -0500

    sorting feature partially works - has some bugs

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ListItem from "./ListItem"
 //set up an initial state for the itemList
 
@@ -7,11 +7,23 @@ import ListItem from "./ListItem"
 
 export default function TodoList({items, checkedItem, removeItems, setItems }) {
 
+    //if sort is true, sort is ascending, if false, its descending
+    const [sort_asc, setSort] = useState('true')
+
+    useEffect(() => {
+        sort_asc ? items.sort((a,b) => (a.value > b.value) ? 1 : -1)
+            :items.sort((a,b) => (a.value < b.value) ? 1 : -1)
+    },[items,sort_asc])
+
+    function toggleSort() {
+        setSort(!sort_asc)
+        console.log(sort_asc)
+    }
 
     return (
         <main>
             <ul id="myUL">
-                <span className="sort-button">
+                <span className="sort-button" onClick={toggleSort}>
                     <i className="fas fa-sort fa-2x"/>
                 </span>
                 {items.map((item, index) => (

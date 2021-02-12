013bffee3087ffa26e0899fd45cde284bec96d7a
BigCityC <courtneywalsh05@gmail.com>
2021-02-12 15:21:34 -0500

    sucessfully combined event handlers for value/date properties

--- src/ListItem.js
+++ src/ListItem.js
@@ -5,61 +5,42 @@ import Pluralize from 'react-pluralize'
 
 export default function ListItem({ item, items, setItems, checkedItem, removeItems}) {
 
-    // const initForm = {
-    //     listText: item.value,
-    //     listDate: item.date
-    // }
+    const initForm = {
+        listText: item.value,
+        listDate: item.date
+    }
 
     const [editable, setIfEditable] = useState(false)
-    const [listText, setListText] = useState(item.value)
-    const [listDate, setListDate] = useState(item.date)
     const [overdue, setOverdue] = useState(checkOverdue())
-    // const [form, setForm] = useState(initForm)
+    const [form, setForm] = useState(initForm)
 
     function checkOverdue() {
         const result = moment().diff(moment(item.date), 'days')
-        console.log(result)
         return (result > 0)
     }
 
     function editItems() {
         setIfEditable(!editable)
-        // item.value = form.listText
-        // item.date = form.listDate
-        setListText(item.value)
-        setListDate(item.date)
+        item.value = form.listText
+        item.date = form.listDate
         setOverdue(checkOverdue())
-        console.log('item.value: ' + item.value + ' ' + item.date)
-        console.log('listitem: ' + listText + ' ' + listDate)
         handleToggleComplete(item.id)
     }
 
-    function handleInputChange(e) {
-        setListText(e.target.value)
+    function handleInputChange(event) {
+        const value = event.target.value
+        const name = event.target.name
+
+        setForm({...form, [name]: value})
     }
-
-    function handleDateChange(e) {
-        setListDate(e.target.value)
-
-    }
-    
-
-    // function handleInputChange(event) {
-    //     const value = event.target.value
-    //     const name = event.target.name
-    //
-    //     setForm({...form, [name]: value})
-    // }
 
     function handleToggleComplete(id) {
         const updatedList = items.map((item) => {
             if (item.id === id) {
                 return {
                     ...item,
-                    // value: form.,
-                    // date: form.listDate,
-                    value: listText,
-                    date: listDate,
+                    value: form.listText,
+                    date: form.listDate,
                 }
             } else {
                 return item
@@ -78,10 +59,8 @@ export default function ListItem({ item, items, setItems, checkedItem, removeIte
             {item.complete && <i className="fas fa-check"/>}
             {editable ?
                 <div className="editable">
-                    <input type="text" value={listText} name="listText" onChange={handleInputChange}/>
-                    <input className="date_input" id="date-input" type="date" value={listDate} name="listDate" onChange={handleDateChange}/>
-                    {/*<input type="text" value={form.listText} name="listText" onChange={handleListChange}/>*/}
-                    {/*<input className="date_input" id="date-input" type="date" value={form.listDate} name="listDate" onChange={handleListChange}/>*/}
+                    <input type="text" value={form.listText} name="listText" onChange={handleInputChange}/>
+                    <input className="date_input" id="date-input" type="date" value={form.listDate} name="listDate" onChange={handleInputChange}/>
                 </div>
 
                 :

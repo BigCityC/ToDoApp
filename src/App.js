71bc7d76a66aeb7f53bfcc7cba950c25e7b7275d
BigCityC <courtneywalsh05@gmail.com>
2021-03-31 13:34:09 -0400

    removed axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import moment from "moment";
-import {FaPlus, FaSort, FaClock} from 'react-icons/fa';
+import { FaPlus, FaSort, FaClock } from 'react-icons/fa';
 import ListItem from "./ListItem";
-import axios from "axios";
 
 
 const currentDay = moment().format("YYYY-MM-DD")
@@ -15,49 +14,12 @@ const initForm = {
 
 const LOCAL_STORAGE_KEY = 'todoApp.items'
 
-
-const api = axios.create({
-    baseURL: 'http://localhost:5000/api/'
-})
-
 function App() {
     const [items, setItems] = useState([])
     const [sortAsc, setSortAsc] = useState(true)
     const [sortBy, setSortBy] = useState('date')
     const [form, setForm] = useState(initForm)
 
-    const getItems = () => {
-        api.get('/items/')
-            .then(function (res) {
-                setItems(res.data)
-            })
-            .catch(function (error) {
-                console.log(error.message);
-            })
-    }
-
-    useEffect(() => {
-        getItems()
-    }, [])
-
-    useEffect(() => {
-        let timer;
-        timer = setTimeout(() => {
-            api.get('items/')
-                .then((res) => {
-                    let items = res.data
-                    const updatedList = items.map((item) => ({
-                            ...item,
-                            overdue: moment(item.date).diff(moment(), 'days')
-                        }))
-                        setItems(updatedList)
-                })
-                .catch(function (error) {
-                    console.log(error.message);
-                })
-            }, 100);
-        return () => clearTimeout(timer);
-    }, [])
 
     useEffect(() => {
         const data = localStorage.getItem(LOCAL_STORAGE_KEY)
@@ -72,8 +34,9 @@ function App() {
                 setItems(updatedList)
             }, 100);
         }
-        return () => clearTimeout(timer);
+        return () => clearInterval(timer);
     }, [])
+
 
     useEffect(() => {
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items))
@@ -84,8 +47,9 @@ function App() {
         if (items.length) {
             const _items = [...items]
             if (sortBy === 'date') {
-                _items.sort((a, b) => a.overdue - b.overdue)
-            } else if (sortBy === 'name') {
+                _items.sort((a, b) => a.overdue-b.overdue)
+            }
+            else if (sortBy === 'name') {
                 _items.sort((a, b) => a.value.localeCompare(b.value))
             }
 
@@ -99,29 +63,11 @@ function App() {
     function handleFormInput(event) {
         const value = event.target.value
         const name = event.target.name
-        setForm({...form, [name]: value})
+        setForm({...form, [name]:value})
     }
 
 
     function addItems() {
-        const item = {
-                    id: uuidv4(),
-                    value: form.task,
-                    date: form.date,
-                    overdue: moment(form.date).diff(moment(currentDay), 'days'),
-                    complete: false
-                };
-        api.post('/items', { item })
-            .then((res) => {
-                setItems(res.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-        setForm(initForm)
-    }
-
-    function addLocalItems() {
         if (form.task) {
             const _items = [...items, {id: uuidv4(), value: form.task, date: form.date, overdue:  moment(form.date).diff(moment(currentDay), 'days'), complete: false}]
             setItems(_items)
@@ -129,6 +75,23 @@ function App() {
             alert('Input cannot be blank, please try again.')
         }
         setForm(initForm)
+    }
+
+    function removeItems(item) {
+        setItems(items.filter(removedItem => removedItem !== item))
+
+    }
+
+
+    function checkedItem(id) {
+        const newTodos = items.map((item) => {
+            if (item.id !== id) {
+                return item
+            } else {
+                return {...item, complete: !item.complete}
+            }
+        })
+        setItems(newTodos)
     }
 
 
@@ -141,80 +104,59 @@ function App() {
         }
     }
 
-    function handleListComplete(data) {
-        let result = data.every((obj)=> obj.complete === true)
-        if (result) {
-            if (window.confirm("All tasks complete! Do you want to clear the tasks?")) {
-                api.delete('/items/delete')
-                    .then(res => {
-                        setItems(res.data)
-                    }).catch((error) => {
-                    console.log(error)
-                })
-            }
-        }
-    }
-
 
     return (
-        <>
-            <header>
-                <h2 className="title">Todo List</h2>
-                <div className="input-wrapper">
-                    <div className="input-bar">
-                        <input id="todolist-input"
-                               placeholder="Input a Task Here..."
-                               type="text"
-                               onChange={handleFormInput}
-                               value={form.task}
-                               name='task'
-                        />
-                        <input id="date-input" type="date" value={form.date} name='date' onChange={handleFormInput}/>
-                    </div>
-                </div>
-                <div className="input-wrapper input-buttons">
+      <>
+          <header>
+              <h2 className="title">Todo List</h2>
+              <div className="input-wrapper">
+                  <div className="input-bar">
+                      <input id="todolist-input"
+                             placeholder="Input a Task Here..."
+                             type="text"
+                             onChange={handleFormInput}
+                             value={form.task}
+                             name='task'
+                      />
+                      <input id="date-input" type="date" value={form.date} name='date' onChange={handleFormInput}/>
+                  </div>
+              </div>
+              <div className="input-wrapper input-buttons">
                         <span
-                            className={`button sort ${sortBy === 'date' && "highlight"}`}
-                            onClick={() => {
-                                sortList('date')
-                            }}>
-                                <FaClock size={30}/>
+                          className={`button sort ${sortBy === 'date' && "highlight"}`}
+                          onClick={() => {sortList('date')}}>
+                                <FaClock size={30} />
                         </span>
 
-                    <span
-                        className="button add" title="Add"
-                        onClick={()=>{
-                            addItems()
-                            addLocalItems()
-                        }}>
+                  <span
+                    className="button add" title="Add"
+                    onClick={addItems}>
                                 <FaPlus size={25}/>
                         </span>
 
-                    <span
-                        className={`button sort ${sortBy === 'name' && "highlight"}`}
-                        onClick={() => {
-                            sortList('name')
-                        }}>
+                  <span
+                    className={`button sort ${sortBy === 'name' && "highlight"}`}
+                    onClick={() => {sortList('name')}}>
                                 <FaSort size={30}/>
                         </span>
-                </div>
-            </header>
+              </div>
+          </header>
 
-            <main>
-                <ul>
-                    {items.map((item, index) => (
-                        <ListItem
-                            key={index}
-                            item={item}
-                            handleListComplete={handleListComplete}
-                            setItems={setItems}
-                            api={api}
-                            items={items}
-                        />
-                    ))}
-                </ul>
-            </main>
-        </>
+          <main>
+              <ul>
+                  {items.map((item, index) => (
+                    <ListItem
+                      key={index}
+                      item={item}
+                      items={items}
+                      checkedItem={checkedItem}
+                      removeItems={removeItems}
+                      setItems={setItems}
+                    />
+                  ))}
+              </ul>
+          </main>
+      </>
 
     )
 }
--- src/ListItem.js
+++ src/ListItem.js
@@ -6,8 +6,7 @@ import Pluralize from 'react-pluralize'
 
 
 
-
-export default function ListItem({ items, item, setItems, handleListComplete, api}) {
+export default function ListItem({ item, items, setItems, checkedItem, removeItems}) {
 
     const initForm = {
         listText: item.value,
@@ -31,124 +30,63 @@ export default function ListItem({ items, item, setItems, handleListComplete, ap
     }
 
     function handleToggleComplete(id) {
-        api.put(`item/${id}`,
-            {
-                value: form.listText,
-                date :form.listDate,
-                overdue: moment(item.date).diff(moment(), 'days')
-            })
-            .then(function (res) {
-                setItems(res.data)
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .finally(function () {
-                const updatedList = items.map((item) => {
-                    if (item.id === id) {
-                        return {
-                            ...item,
-                            value: form.listText,
-                            date: form.listDate,
-                            overdue: moment(item.date).diff(moment(), 'days'),
-                        }
-                    } else {
-                        return item
-                    }
-                })
-                setItems(updatedList)
-
-            })
-    }
-
-    function removeItem(item){
-        api.delete(`/item/${item.id}`)
-            .then(res => {
-                setItems(res.data)
-                console.log(res.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
-
-    function removeLocalItem(item) {
-        setItems(items.filter(removedItem => removedItem !== item))
-
-    }
-
-    function checkedItem(id) {
-        api.put(`item/${id}/complete`,
-            {complete:!item.complete})
-            .then((res)=>{
-                setItems(res.data)
-                handleListComplete(res.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-
-    }
-
-    function checkedLocalItem(id) {
-        const newTodos = items.map((item) => {
-            if (item.id !== id) {
-                return item
+        const updatedList = items.map((item) => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    value: form.listText,
+                    date: form.listDate,
+                    overdue: moment(item.date).diff(moment(), 'days'),
+                }
             } else {
-                return {...item, complete: !item.complete}
+                return item
             }
         })
-        setItems(newTodos)
+        setItems(updatedList)
+
     }
 
     return (
-        <li key={item.id}>
-            {item.complete && <FaCheck className="list-check"/>}
-            {editable ?
-                <div className="list-item editable">
-                    {form.listText.length <= 23 ?
-                        <input type="text" value={form.listText} name="listText" onChange={handleInputChange}/>
-                        :
-                        <textarea value={form.listText} name="listText" onChange={handleInputChange}/>
-                    }
+      <li key={item.id}>
+          {item.complete && <FaCheck className="list-check"/>}
+          {editable ?
+            <div className="list-item editable">
+                {form.listText.length <= 23 ?
+                  <input type="text" value={form.listText} name="listText" onChange={handleInputChange}/>
+                  :
+                  <textarea value={form.listText} name="listText" onChange={handleInputChange}/>
+                }
 
-                    <input className="date_input" id="date-input" type="date" value={form.listDate} name="listDate" onChange={handleInputChange}/>
-                </div>
+                <input className="date_input" id="date-input" type="date" value={form.listDate} name="listDate" onChange={handleInputChange}/>
+            </div>
 
-                :
-                <div className="list-item not_editable">
-                    <p className={item.complete ? "checked" : ""} onClick={() =>
-                    {
-                        checkedItem(item.id)
-                        checkedLocalItem(item.id)
-                    }}>
-                        {item.value}
-                    </p>
+            :
+            <div className="list-item not_editable">
+                <p className={item.complete ? "checked" : ""} onClick={() => {checkedItem(item.id)}}>
+                    {item.value}
+                </p>
 
-                    {(item.overdue < 0) ?
-                        <span className="overdue date_item">
+                {(item.overdue < 0) ?
+                  <span className="overdue date_item">
                             Overdue by <Pluralize singular={'day'} count={Math.abs(item.overdue)}/>
                         </span>
-                    :
-                        <span className="ontime date_item">
+                  :
+                  <span className="ontime date_item">
                              {`Due:  ${moment(item.date).format("MMM Do, YYYY")}`}
                         </span>
-                    }
+                }
 
-                </div>
-            }
-            <div className="list-buttons">
+            </div>
+          }
+          <div className="list-buttons">
                 <span className="list-action list-edit" onClick={() => editItems()}>
                     <FaEdit className="icon" size={20} title="Edit" />
 
                 </span>
-                <span className="list-action list-delete"  onClick={() => {
-                    removeItem(item)
-                    removeLocalItem(item)
-                }}>
+              <span className="list-action list-delete"  onClick={() => {removeItems(item)}}>
                     <FaTrashAlt className="icon" size={20} title="Delete" />
                 </span>
-            </div>
-        </li>
+          </div>
+      </li>
     )
 }
